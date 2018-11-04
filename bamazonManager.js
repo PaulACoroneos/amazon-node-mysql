f29bc8346524de34f44d7b5bb3520e6d870ddf18
Paul Coroneos <paulalexandercoroneos@gmail.com>
2018-11-04 16:59:54 -0600

    first challenge section done

--- bamazonManager.js
+++ bamazonManager.js
@@ -40,12 +40,16 @@ function menu() {
         switch(answers.selection) {
             case "View Products for Sale" :
                 productsSale();
+                break;
             case "View Low Inventory" :
                 lowInventory();
+                break;
             case "Add to Inventory" :
                 addInventory();
+                break;
             case "Add New Product" :
                 addProduct();
+                break;
             case "Exit" :
                 break;
             default:
@@ -88,38 +92,119 @@ function lowInventory() {
 }
 
 function addInventory() {
-    connection.query(
-        "SELECT * from products",
-        function(err, res) {
-            if (err) throw err;
-            //Lets make the data look nice
-            //console.log(res);
-            console.log("Inventory with less than 5 items.\n")
-            for(let i=0; i<res.length;i++) {
-                if(res[i].stock_quantity <5)
-                    console.log("itemID: ",res[i].itemID,"\nProduct Name: ",res[i].product_name,"\nDepartment Name: ",res[i].department_name,"\nPrice: ",res[i].price,"\nStock Quantity: ",res[i].stock_quantity,"\n");
-            }
-            connection.end();
+
+    inquirer
+    .prompt( [
+        { 
+            name: 'select',
+            type: 'input',
+            message: 'Please select which itemId you would like to add inventory to.',
         }
-    );
-}
+    ]).then (answers => {
+
+        let itemUpdate = answers.select;
+        console.log("item to update ",itemUpdate);
+        inquirer
+        .prompt( [
+            { 
+                name: 'newStock',
+                type: 'input',
+                message: 'And what quantity would you like to add?',
+            }
+        ]).then (answers => {
+
+            let newStock = parseInt(answers.newStock);
+            //console.log("new stock ",newStock);
+            //okay getting existing inventory on selected itemID
+            var query1 = connection.query(
+                "SELECT itemID,stock_quantity FROM products", function(err, res) {
+                    if (err) throw err;
+                    // Log all results of the SELECT statement
+                    console
+                    let existingStock = parseInt(res[itemUpdate-1].stock_quantity);
+                    //console.log("existing stock ",existingStock);
+                    newStock += existingStock;
+                    var query2 = connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                            stock_quantity: newStock
+                            },
+                            {
+                            itemID: itemUpdate
+                            }
+                        ],
+                        function(err, res) {
+                            if (err) throw err;
+                            console.log(res.affectedRows + " products updated!\n","New stock for item is: ",newStock);
+                        }
+                        
+                    );
+                    connection.end();
+                });
+            });
+        });
+    }
 
 function addProduct() {
+    let productDepartment,productName,productQuantity,productPrice;
+    console.log("hi");
     inquirer
-    .prompt
-    var query = connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [
-        {
-        quantity: 100
-        },
-        {
-        flavor: "Rocky Road"
-        }
-    ],
-    function(err, res) {
-        console.log(res.affectedRows + " products updated!\n");
-        // Call deleteProduct AFTER the UPDATE completes
-        deleteProduct();
-    }
+    .prompt (
+        [
+            {
+                type:'input',
+                message:'Type the name of the product you want to add',
+                name:'productName'
+            }
+    ]).then (answers => {
+        productName = answers.productName;
+        inquirer
+        .prompt (
+        [
+            {
+                type:'input',
+                message:'Type the price of the product',
+                name:'productPrice'
+            }
+        ]).then (answers => {
+            productPrice = answers.productPrice;
+            inquirer
+            .prompt (
+            [
+                {
+                    type:'input',
+                    message:'Type the initial product quantity',
+                    name:'productQuantity'
+                }
+            ]).then (answers => {
+                productQuantity = answers.productQuantity;
+                inquirer
+                .prompt (
+                [
+                    {
+                        type:'input',
+                        message:'Type the product department',
+                        name:'productDepartment'
+                    }
+                ]).then (answers => {
+                    productDepartment = answers.productDepartment;
+                    console.log("Inserting a new product...\n");
+                    var query = connection.query(
+                        "INSERT INTO products SET ?",
+                        {
+                            stock_quantity: productQuantity,
+                            department_name: productDepartment,
+                            product_name: productName,
+                            price: productPrice
+                        },
+                        function(err, res) {
+                            console.log(res.affectedRows + " product inserted!\n");
+                            connection.close();
+                        }
+                    );
+                });
+            });
+        });   
+    });
 }
